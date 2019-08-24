c5c6c796933c44da33ca542f1fbb26219a032262
Rakshith Vasudev <rakshithvbharani@gmail.com>
2019-08-24 12:34:28 -0500

    adding training script

--- dllibnet/train.py
+++ dllibnet/train.py
@@ -5,4 +5,24 @@ Function to train a nn
 from dllibnet.tensor import Tensor
 from dllibnet.nn import NeuralNet
 from dllibnet.loss import Loss, MSE
+from dllibnet.optim import Optimizer, SGD
+from dllibnet.data import BatchIterator, DataIterator
 
+
+def train(net: NeuralNet,
+          inputs: Tensor,
+          targets: Tensor,
+          num_epochs: int = 5000,
+          iterator: DataIterator = BatchIterator(),
+          loss: Loss = MSE(),
+          optimizer: Optimizer = SGD()) -> None:
+
+    for epoch in range(num_epochs):
+        epoch_loss = 0.0
+        for batch in iterator(inputs, targets):
+            predicted = net.forward(batch.inputs)
+            epoch_loss += loss.loss(predicted, batch.targets)
+            grad = loss.grad(predicted, batch.targets)
+            net.backward(grad)
+            optimizer.step(net)
+        print(epoch, epoch_loss)
