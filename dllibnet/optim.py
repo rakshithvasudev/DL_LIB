e76589a335ce367ff2481c60401bc1b8ff56e119
Rakshith Vasudev <rakshithvbharani@gmail.com>
2019-08-23 20:32:51 -0500

    addedbatch iterator

--- dllibnet/optim.py
+++ dllibnet/optim.py
@@ -4,6 +4,7 @@ Optimizer adjusts parameters based on Gradients during backprop.
 
 from dllibnet.nn import NeuralNet
 
+
 class Optimizer:
     def step(self, NeuralNet):
         raise NotImplemented
@@ -13,7 +14,6 @@ class SGD(Optimizer):
     def __init__(self, lr: float = 0.1) -> None:
         self.lr = lr
 
-    def step(self, net:NeuralNet) -> None:
+    def step(self, net: NeuralNet) -> None:
         for param, grad in net.params_and_grads():
             param -= self.lr * grad
-
